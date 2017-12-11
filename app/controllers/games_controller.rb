877160b638783ac2ca3ffd87b03616a853b54b06
Josue Figueroa <JosueFigueroa@Macs-MacBook-Pro.local>
2017-12-11 15:01:29 -0600

    Removed debugger

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,7 +10,6 @@ class GamesController < ApplicationController
 
   def create
     @game = Game.new(game_params)
-    debugger
     @game.user_id = current_user.id
     if @game.save
       redirect_to games_path, notice: 'Game was created.'
